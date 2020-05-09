3615e757d4565dcc87702c4b7ac3f25a8b61c94f
bagi3 <65060927+bagi3@users.noreply.github.com>
2020-05-09 18:56:23 +0900

    up

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,26 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+    
+ID = 708531208472100934
+@bot.event
+async def on_ready():
+    print("Logged in us")
+    print(client.user.name)
+    print(client.user.id)
+    print("-----")
+@bot.event
+async def on_raw_reaction_add(payload):
+    if payload.message_id == ID:
+        print(payload.emoji.name)
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
+        if role is not None:
+            print(role.name + " was found!")
+            print(role.id)
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            await member.add_roles(role)
+            print("done")
 
 bot.run(token)
