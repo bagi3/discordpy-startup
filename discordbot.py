92b2479e0d05c4f93c0302ea9a85849b0e140bfd
bagi3 <65060927+bagi3@users.noreply.github.com>
2020-05-09 16:02:19 +0900

    Update

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,31 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
+TOKEN = "NzA4NTE1OTM5Mjk3MTMyNTY2.XrYe_A.gBLVFt1koHlB8Xg2MZCEB5Jaymw"
+client = discord.Client()
+ID = 708531208472100934
+@client.event
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+async def on_raw_reaction_add(payload):
+    if payload.message_id == ID:
+        print(payload.emoji.name)
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
+        if role is not None:
+            print(role.name + " was found!")
+            print(role.id)
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            await member.add_roles(role)
+            print("done")
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+@client.event
+async def on_raw_reaction_remove(payload):
+    if payload.message_id == ID:
+        print(payload.emoji.name)
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
+        if role is not None:
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            await member.remove_roles(role)
+            print("done")
+client.run(TOKEN)
