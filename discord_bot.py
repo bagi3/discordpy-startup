9f228fa5898eb46ee7ad9dda9a85566a6ee6216f
bagi3 <65060927+bagi3@users.noreply.github.com>
2020-05-09 16:28:33 +0900

    update

--- discord_bot.py
+++ discord_bot.py
@@ -22,15 +22,4 @@ async def on_raw_reaction_add(payload):
             await member.add_roles(role)
             print("done")
 
-@client.event
-async def on_raw_reaction_remove(payload):
-    if payload.message_id == ID:
-        print(payload.emoji.name)
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
-        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
-        if role is not None:
-            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
-            await member.remove_roles(role)
-            print("done")
 client.run(TOKEN)
